4492e41900a617baea1c27ec9c940b1df93f5181
TomaAndreea22 <40408583+TomaAndreea22@users.noreply.github.com>
2018-06-21 13:27:24 +0300

    Update Counter.v

--- Counter.v
+++ Counter.v
@@ -1,12 +1,14 @@
 module Counter(
 input clk,
 input rst,
-output [7:0] Q
+output reg [7:0] out
 ); 
 
 always@(posedge clk) begin
 	if(rst == 1'b1)
 		out <= 8'b0;
 			else
-		out <= out + 1'b1;
+		out <= out + 8'b00000001;
 end
+
+endmodule
