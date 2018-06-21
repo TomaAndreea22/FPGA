408c86b7cdb95c2ea2674e424d167d07aaba18db
TomaAndreea22 <40408583+TomaAndreea22@users.noreply.github.com>
2018-06-21 13:29:48 +0300

    Update VGA.v

--- VGA.v
+++ VGA.v
@@ -1,35 +1,54 @@
 module VGA(
 	input clk_in,
+	input clk_div,
 	input rst,
-	input [7:0] R, G, B,
-	output [7:0] R_t, G_t, B_t,
-	input [11:0] xpose, ypose,
+	output [11:0] xpose, ypose,
 	output hsync, vsync,
-	output vga_blck_n, vga_sync_n,
-	input disp_active
+	output vga_black_n,
+	output disp_active,
+	output reg [3:0] MODE
 );
 
 reg [9:0] h_count;
 reg [9:0] v_count;
-localparam horizontal_sync_start;
-localparam vertical_sync_start;
+
 localparam LINE = 640;
 localparam SCREEN = 480;
+localparam HORIZONTAL_SYNC_START = 16;
+localparam HORIZONTAL_SYNC_END = 16 + 96; 
+localparam HA_STA = 16 + 96 + 48;
+localparam VERTICAL_SYNC_STA = 480 + 11;
+localparam VERTICAL_SYNC_END = 480 + 11 + 2;
+localparam VA_END = 480;
+
+assign hsync = ~((h_count >= HORIZONTAL_SYNC_START)&&(h_count < HORIZONTAL_SYNC_END));
+assign vsync = ((v_count >= VERTICAL_SYNC_STA )&&(v_count < VERTICAL_SYNC_END));
+
+assign xpose = (h_count < HA_STA) ? 0 : (h_count - HA_STA);
+assign ypose = (v_count >= VA_END) ? (VA_END - 1) : (v_count);
+
+assign vga_black_n = ((h_count < HA_STA) | (v_count > VA_END - 1));
+assign disp_active = ~((h_count < HA_STA) | (v_count > VA_END - 1)); 
+
 always@(posedge clk_in) begin
 
 	if(rst == 0) begin
 		h_count <= 0;
 		v_count <= 0;
-			if (clk_in) begin 
-				if(h_count == LINE)begin
-					 h_count <= 0;
-					 v_count <= v_count +1;
-				end
-				else 
+	end
+	if(clk_div) begin 
+		if(h_count == LINE) begin 
+					h_count <= 0;
+					v_count <= v_count + 1;
+					end
+					else 
 					h_count <= h_count + 1;
 				if(v_count == SCREEN)
 					v_count <= 0;
 				end
+	MODE <= MODE + 4'b0001;
 		
 	end
+
+endmodule
 	
